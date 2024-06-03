7eca0fa4ee70778f22297820f3cdc0f20f1277de
iamyourdre <iamyourdre@gmail.com>
2024-06-03 22:34:31 +0700

    feat: frontend oop on SrjkForm.jsx

--- frontend/src/components/SrjkForm.jsx
+++ frontend/src/components/SrjkForm.jsx
@@ -1,17 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { FaCircleInfo } from "react-icons/fa6";
-import axios from 'axios';
+import ProbDataHandler from "../contexts/ProbDataHandler";
 
 const SrjkForm = () => {
-
-  const mapels = ["PABP", "PPKN", "B.Indonesia", "MTK Wajib", "Sejarah Indonesia", "B.Inggris Wajib", "Seni Budaya", "PJOK", "PKWU", "MTK Peminatan", "Biologi", "Fisika", "Kimia", "Ekonomi", "B.Inggris Terapan"];
-  const semesters = ["1", "2", "3", "4", "5"];
+  const handler = new ProbDataHandler();
+  const { mapels, semesters } = handler;
 
   const [formData, setFormData] = useState(getInitialFormData());
   const [averageScores, setAverageScores] = useState({});
   const [probData, setProbData] = useState([]);
 
-  // Mengambil nilai-nilai dari cookie saat komponen dimuat dan menginisialisasi state
   function getInitialFormData() {
     const cookieData = document.cookie.split(';').reduce((acc, cookie) => {
       const [key, value] = cookie.split('=');
@@ -23,77 +21,66 @@ const SrjkForm = () => {
     return cookieData;
   }
 
-  // Fungsi untuk mengubah kunci sesuai dengan format yang diharapkan
-  function formatKey(semester, mapel) {
-    return `s${semester}_${mapel.replace(/\s+/g, '_')}`;
-  }
-  
-  // Fungsi untuk menangani paste dari clipboard
   function handlePaste(event) {
     event.preventDefault();
     const clipboardData = event.clipboardData || window.clipboardData;
     const pastedData = clipboardData.getData('text');
     const scores = pastedData.split('\t').map(score => parseInt(score.trim(), 10));
 
-    // Menetapkan nilai-nilai yang dipaste ke input sesuai dengan urutannya
-    let updatedFormData = { ...formData }; // Copy the current state
+    let updatedFormData = { ...formData };
 
     let index = 0;
     semesters.forEach((semester, semesterIndex) => {
       mapels.forEach((mapel, mapelIndex) => {
         if (scores[index] !== undefined) {
-          if(semester > 2 && mapelIndex === 14){
-            const key = formatKey(semester, mapel);
+          if (semester > 2 && mapelIndex === 14) {
+            const key = handler.formatKey(semester, mapel);
             updatedFormData[key] = 0;
-            handleInputChange(semester, mapel, updatedFormData[key])
+            handleInputChange(semester, mapel, updatedFormData[key]);
             index--;
           } else {
-            const key = formatKey(semester, mapel);
+            const key = handler.formatKey(semester, mapel);
             updatedFormData[key] = scores[index];
-            handleInputChange(semester, mapel, updatedFormData[key])
+            handleInputChange(semester, mapel, updatedFormData[key]);
           }
         }
         index++;
       });
     });
 
-    // Set the updated state
     setFormData(updatedFormData);
   }
 
-  // Fungsi untuk menyimpan nilai input ke state dan cookie
   function handleInputChange(semester, mapel, value) {
     const expires = new Date();
     expires.setDate(expires.getDate() + 7);
-  
-    const cookieKey = formatKey(semester, mapel);
+
+    const cookieKey = handler.formatKey(semester, mapel);
     const cookieValue = `${cookieKey}=${value}; expires=${expires.toUTCString()}; path=/`;
-  
+
     const newFormData = { ...formData, [cookieKey]: value };
     setFormData(newFormData);
     document.cookie = cookieValue;
   }
 
-  // Fungsi untuk mereset semua nilai input dan hapus cookie
   function resetAllValues() {
     const newFormData = {};
     setFormData(newFormData);
     mapels.forEach(mapel => {
       semesters.forEach(semester => {
-        document.cookie = `${formatKey(semester, mapel)}=; path=/;`;
+        document.cookie = `${handler.formatKey(semester, mapel)}=; path=/;`;
       });
     });
   }
 
-  // Fungsi untuk menghitung rata-rata nilai setiap mata pelajaran
   function calculateAverageScores() {
     const averages = {};
     mapels.forEach(mapel => {
       let totalScore = 0;
       let count = 0;
       semesters.forEach(semester => {
-        const score = parseFloat(formData[formatKey(semester, mapel)]);
-        if (score>0) {
+        const score = parseFloat(formData[handler.formatKey(semester, mapel)]);
+        if (score > 0) {
           totalScore += score;
           count++;
         }
@@ -105,80 +92,11 @@ const SrjkForm = () => {
     return averages;
   }
 
-  // Fungsi untuk menangani submit
   function handleSubmit() {
-
     const avgSc = calculateAverageScores();
-
-    const averageScoresArray = Object.values(avgSc);
-
-    // Membuat objek requestBody sesuai dengan bentuk yang diharapkan oleh API
-    const requestBody = {
-      x1: averageScoresArray[0],
-      x2: averageScoresArray[1],
-      x3: averageScoresArray[2],
-      x4: averageScoresArray[3],
-      x5: averageScoresArray[4],
-      x6: averageScoresArray[5],
-      x7: averageScoresArray[6],
-      x8: averageScoresArray[7],
-      x9: averageScoresArray[8],
-      x10: averageScoresArray[9],
-      x11: averageScoresArray[10],
-      x12: averageScoresArray[11],
-      x13: averageScoresArray[12],
-      x14: averageScoresArray[13],
-      x15: averageScoresArray[14]
-    };
-
-    // Setelah setState probData dengan response dari server
-    axios.post('http://localhost:5000/api/nb/naiveBayesClassifier', requestBody)
-      .then(response => {
-        // Mendapatkan probData dari response
-        const probDataFromServer = response.data.probData;
-        
-        // Mengurutkan probData berdasarkan p_yes secara descending
-        const sortedProbData = probDataFromServer.sort((a, b) => b.p_yes - a.p_yes);
-        
-        // Menetapkan probData yang sudah diurutkan ke state
-        setProbData(sortedProbData);
-
-        // Mencari elemen dengan id "rekomendasi"
-        const rekomendasiElement = document.getElementById('rekomendasi');
-        // Memastikan elemen ditemukan sebelum mencoba untuk mengarahkan scroll
-        if (rekomendasiElement) {
-          // Mengarahkan scroll ke elemen "rekomendasi" secara smooth
-          rekomendasiElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        }
-      })
-      .catch(error => {
-        console.error('Error submitting average scores:', error);
-      });
+    handler.submitAverageScores(avgSc, setProbData);
   }
 
-  function convertToGrade(score) {
-    const numericScore = parseFloat(score)
-    if (numericScore >= 90) {
-      return "A";
-    } else if (numericScore >= 85) {
-      return "A-";
-    } else if (numericScore >= 80) {
-      return "B+";
-    } else if (numericScore >= 75) {
-      return "B";
-    } else if (numericScore >= 70) {
-      return "B-";
-    } else {
-      return "CDE";
-    }
-  }
-
-  function biggerOrSame(grade1, grade2) {
-    const gradeOrder = ["A", "A-", "B+", "B", "B-", "CDE"];
-    const index1 = gradeOrder.indexOf(grade1);
-    const index2 = gradeOrder.indexOf(grade2);
-    return index1 <= index2;
-  }
   return (
     <div>
       <div className="px-4 md:px-8">
@@ -187,7 +105,7 @@ const SrjkForm = () => {
             <div>
               <div role="alert" className="alert text-left bg-green-500 mb-3 inline-block rounded-md text-white text-sm">
                 <span className="label text-xl font-bold mb-2">Cara Menggunakan:</span>
-                <ol class="list-decimal pl-4 mt-2">
+                <ol className="list-decimal pl-4 mt-2">
                   <li> Isi nilai-nilai tiap semester berdasarkan rapor Anda pada kolom input yang disediakan.</li>
                   <li> Koreksi ulang nilai-nilai yang telah anda masukkan demi mencegah kesalahan rekomendasi jurusan.</li>
                   <li> Tekan tombol "Submit" setelah semua nilai dimasukkan.</li>
@@ -213,11 +131,11 @@ const SrjkForm = () => {
                           value={
                             semester > 2 && mapel === "B.Inggris Terapan" 
                               ? '0'
-                              : formData[formatKey(semester, mapel)] || ''
+                              : formData[handler.formatKey(semester, mapel)] || ''
                           }
                           onChange={(e) => handleInputChange(semester, mapel, e.target.value)}
                           onPaste={handlePaste}
-                          id={formatKey(semester, mapel)}
+                          id={handler.formatKey(semester, mapel)}
                         />
                       </div>
                     </label>
@@ -244,61 +162,62 @@ const SrjkForm = () => {
               <FaCircleInfo className='inline text-md relative bottom-0.5 mr-2'/>
               Berdasarkan hasil klasifikasi, <b>anda direkomendasikan untuk masuk ke jurusan <u>{probData[0].jurusan.jurusan}</u></b>. Untuk dijadikan pertimbangan, silahkan cek detail dan rekomendasi lainnya pada tabel dibawah ini. 
             </div>
-              <div className="overflow-x-auto w-full">      
-                <table className="w-full table-xs border-collapse border border-gray-400">
-                  <tbody>
-                    <tr>
-                      <th className="border border-gray-400 px-4 py-2" rowSpan='2'>Rekomendasi</th>
-                      <th className="border border-gray-400 px-4 py-2" colSpan='15'>Rata-Rata Bobot</th>
-                      <th className="border border-gray-400 bg-p-dark text-white px-4 py-2" rowSpan='3'>Rasio Kualifikasi</th>
-                      <th className="border border-gray-400 bg-p-dark text-white px-4 py-2" rowSpan='3'>Probabilitas Klasifikasi</th>
-                    </tr>
-                    <tr>
-                      {mapels.map((mapel) => (
-                        <td className="border border-gray-400 px-4 py-2 font-bold [writing-mode:vertical-lr]">{mapel}</td>
-                      ))}
-                    </tr>
-                    <tr className="bg-p-dark text-white">
-                      <td className="border border-gray-400 px-4 py-2"><b>NILAI ANDA</b></td>
-                      {mapels.map((mapel) => (
-                        <td className="border border-gray-400 px-4 py-2">{convertToGrade(averageScores[mapel])}</td>
-                      ))}
-                    </tr>
-                    {probData.map((pData) => (
-                      pData.reference.map((rData, index) => {
-                        let count = 0;
-                        return(
-                          <tr key={index}>
-                            <td className="border border-gray-400 px-4 py-2">
-                              <ul>
-                                <li className="font-bold">{rData.jurusan_key.jurusan}</li>
-                                <li>{rData.univ_key.universitas}</li>
-                                <li>{rData.nama} </li>
-                              </ul>
-                            </td>
-                            {Object.keys(rData.summary_key[0]).map((key, idx) => {
-                              const status = biggerOrSame(convertToGrade(averageScores[mapels[idx-2]]), convertToGrade(rData.summary_key[0][key]));
-                              if(status){count++};
-                              return (
-                              key.startsWith('mean_') &&
-                              <td className={`border border-gray-400 px-4 py-2 font-semibold ${status?'bg-green-300':'bg-red-300'}`} key={idx}>
-                                {convertToGrade(rData.summary_key[0][key])}
-                              </td>
-                            )})}
-                            <td className="border border-gray-400 px-4 py-2 font-bold">{(((count-2) / 15) * 100).toFixed(2)}%</td>
-                            <td className="border border-gray-400 px-4 py-2 font-bold">{pData.p_yes}</td>
-                          </tr>
-                        )
-                      })
+            <div className="overflow-x-auto w-full">      
+              <table className="w-full table-xs border-collapse border border-gray-400">
+                <tbody>
+                  <tr>
+                    <th className="border border-gray-400 px-4 py-2" rowSpan='2'>Rekomendasi</th>
+                    <th className="border border-gray-400 px-4 py-2" colSpan='15'>Rata-Rata Bobot</th>
+                    <th className="border border-gray-400 bg-p-dark text-white px-4 py-2" rowSpan='3'>Rasio Kualifikasi</th>
+                    <th className="border border-gray-400 bg-p-dark text-white px-4 py-2" rowSpan='3'>Probabilitas Klasifikasi</th>
+                  </tr>
+                  <tr>
+                    {mapels.map((mapel) => (
+                      <td className="border border-gray-400 px-4 py-2 font-bold [writing-mode:vertical-lr]" key={mapel}>{mapel}</td>
                     ))}
-                  </tbody>
-                </table>
-              </div>
+                  </tr>
+                  <tr className="bg-p-dark text-white">
+                    <td className="border border-gray-400 px-4 py-2"><b>NILAI ANDA:</b></td>
+                    {mapels.map((mapel) => (
+                      <td className="border border-gray-400 px-4 py-2" key={mapel}>{handler.convertToGrade(averageScores[mapel])}</td>
+                    ))}
+                  </tr>
+                  {probData.map((pData) => (
+                    pData.reference.map((rData, index) => {
+                      let count = 0;
+                      return (
+                        <tr key={index}>
+                          <td className="border border-gray-400 px-4 py-2">
+                            <ul>
+                              <li className="font-bold">{rData.jurusan_key.jurusan}</li>
+                              <li>{rData.univ_key.universitas}</li>
+                              <li>{rData.nama}</li>
+                            </ul>
+                          </td>
+                          {Object.keys(rData.summary_key[0]).map((key, idx) => {
+                            const status = handler.biggerOrSame(handler.convertToGrade(averageScores[mapels[idx-2]]), handler.convertToGrade(rData.summary_key[0][key]));
+                            if (status) { count++; }
+                            return (
+                              key.startsWith('mean_') &&
+                              <td className={`border border-gray-400 px-4 py-2 font-semibold ${status ? 'bg-green-300' : 'bg-red-300'}`} key={idx}>
+                                {handler.convertToGrade(rData.summary_key[0][key])}
+                              </td>
+                            );
+                          })}
+                          <td className="border border-gray-400 px-4 py-2 font-bold">{(((count-2) / 15) * 100).toFixed(2)}%</td>
+                          <td className="border border-gray-400 px-4 py-2 font-bold">{pData.p_yes}</td>
+                        </tr>
+                      );
+                    })
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default SrjkForm
+export default SrjkForm;
